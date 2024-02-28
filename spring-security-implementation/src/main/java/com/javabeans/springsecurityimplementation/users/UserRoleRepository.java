50eaa3549fbdd6b08da1f8bc9636f0057858455f
Md. Mehedi Hasan <mehedi32.cse.hstu@gmail.com>
2024-02-28 15:36:34 +0600

    Removed dupplicate project files that were created by mistakes.

--- spring-security-implementation/src/main/java/com/javabeans/springsecurityimplementation/users/UserRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javabeans.springsecurityimplementation.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-}
